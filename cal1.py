d4f90c95bde54d8026a068950b6a40eae5d39845
RezaSch <reza.s2001microsoft@gmail.com>
2024-05-18 18:53:38 +0330

    add multiply to the code

--- cal1.py
+++ cal1.py
@@ -2,16 +2,21 @@
 def add(x, y):
     return x + y
 
+# This function adds two numbers
+def multiply(x, y):
+    return x * y
+
 print("Select operation:")
 print("1. Add")
+print("3. multiply")
 
 
 while True:
     # Take input from the user
-    choice = input("Enter choice (1): ")
+    choice = input("Enter choice (1/3): ")
 
     # Check if choice is one of the four options
-    if choice in ('1'):
+    if choice in ('1','2'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -21,6 +26,8 @@ while True:
 
         if choice == '1':
             print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif choice == '3':
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
 
         # Check if the user wants another calculation
         next_calculation = input("Let's do the next calculation? (yes/no): ")
