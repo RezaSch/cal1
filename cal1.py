b4bcb666180822f65b0347f1c21583d78e48898b
RezaSch <reza.s2001microsoft@gmail.com>
2024-05-18 18:44:38 +0330

    add sum to the code

--- cal1.py
+++ cal1.py
@@ -2,21 +2,16 @@
 def add(x, y):
     return x + y
 
-# This function adds two numbers
-def subtract(x, y):
-    return x - y
-
 print("Select operation:")
 print("1. Add")
-print("2. Subtract")
 
 
 while True:
     # Take input from the user
-    choice = input("Enter choice (1/2): ")
+    choice = input("Enter choice (1): ")
 
     # Check if choice is one of the four options
-    if choice in ('1','2'):
+    if choice in ('1'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -26,8 +21,6 @@ while True:
 
         if choice == '1':
             print(f"{num1} + {num2} = {add(num1, num2)}")
-        elif choice == '2':
-            print(f"{num1} - {num2} = {subtract(num1, num2)}")
 
         # Check if the user wants another calculation
         next_calculation = input("Let's do the next calculation? (yes/no): ")
